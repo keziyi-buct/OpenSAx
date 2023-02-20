00075dab1f2e8d4eba01f8132f498f6e3566511f
keziyi-buct <840037511@qq.com>
2023-02-20 20:16:49 +0800

    commit

---
+++ github.sh
@@ -0,0 +1,7 @@
+cd /root/OpenSA4/
+git init
+git add /root/OpenSA4/
+git commit -m "commit"
+git branch -M main
+git remote add origin https://github.com/keziyi-buct/soil_OpenSAx.git
+git push -u origin main
